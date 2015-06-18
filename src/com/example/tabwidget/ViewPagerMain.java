0db7c23a2962eb687a9730abf9753363183210e1
FlyfishLei <lemontanglei@163.com>
2015-06-18 17:52:13 +0800

    透明度

--- src/com/example/tabwidget/ViewPagerMain.java
+++ src/com/example/tabwidget/ViewPagerMain.java
@@ -14,6 +14,7 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.TranslateAnimation;
@@ -24,134 +25,136 @@ import android.widget.TextView;
 
 public class ViewPagerMain extends Activity {
 
-	private ViewPager mPager;//页卡内容
-    private List<View> listViews; // Tab页面列表
-    private ImageView cursor;// 动画图片
-    private TextView t1, t2, t3;// 页卡头标
-    private int offset = 0;// 动画图片偏移量
-    private int currIndex = 0;// 当前页卡编号
-    private int bmpW;// 动画图片宽度
+	private ViewPager mPager;// 页卡内容
+	private List<View> listViews; // Tab页面列表
+	private ImageView cursor;// 动画图片
+	private TextView t1, t2, t3;// 页卡头标
+	private int offset = 0;// 动画图片偏移量
+	private int currIndex = 0;// 当前页卡编号
+	private int bmpW;// 动画图片宽度
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		InitTextView();
-		
+
 		InitImageView();
 		InitViewPager();
 	}
+
 	/**
-     * 初始化头标
-*/
-    private void InitTextView() {
-        t1 = (TextView) findViewById(R.id.text1);
-        t2 = (TextView) findViewById(R.id.text2);
-        t3 = (TextView) findViewById(R.id.text3);
+	 * 初始化头标
+	 */
+	private void InitTextView() {
+		t1 = (TextView) findViewById(R.id.text1);
+		t2 = (TextView) findViewById(R.id.text2);
+		t3 = (TextView) findViewById(R.id.text3);
 
-        t1.setOnClickListener(new MyOnClickListener(0));
-        t2.setOnClickListener(new MyOnClickListener(1));
-        t3.setOnClickListener(new MyOnClickListener(2));
-    }
-    
-    /**
-     * 初始化ViewPager
-*/
-    private void InitViewPager() {
-        mPager = (ViewPager) findViewById(R.id.vPager);
-        listViews = new ArrayList<View>();
-        LayoutInflater mInflater = getLayoutInflater();
-        listViews.add(mInflater.inflate(R.layout.lay1, null));
-        listViews.add(mInflater.inflate(R.layout.lay2, null));
-        listViews.add(mInflater.inflate(R.layout.lay3, null));
-        mPager.setAdapter(new MyPagerAdapter(listViews));
-        mPager.setCurrentItem(0);
-        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
-    }
-    
-    /**
-     * 初始化动画
-*/
-    private void InitImageView() {
-        cursor = (ImageView) findViewById(R.id.cursor);
-        bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher).getWidth();// 获取图片宽度
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenW = dm.widthPixels;// 获取分辨率宽度
-        offset = (screenW / 3 - bmpW) / 2;// 计算偏移量
-        Matrix matrix = new Matrix();
-        matrix.postTranslate(offset, 0);
-        //matrix.postTranslate(0, 0);
-        cursor.setImageMatrix(matrix);// 设置动画初始位置
-    }
-    
-    /**
-     * 头标点击监听
-*/
-    public class MyOnClickListener implements View.OnClickListener {
-        private int index = 0;
+		t1.setOnClickListener(new MyOnClickListener(0));
+		t2.setOnClickListener(new MyOnClickListener(1));
+		t3.setOnClickListener(new MyOnClickListener(2));
+	}
 
-        public MyOnClickListener(int i) {
-            index = i;
-        }
+	/**
+	 * 初始化ViewPager
+	 */
+	private void InitViewPager() {
+		mPager = (ViewPager) findViewById(R.id.vPager);
+		listViews = new ArrayList<View>();
+		LayoutInflater mInflater = getLayoutInflater();
+		listViews.add(mInflater.inflate(R.layout.lay1, null));
+		listViews.add(mInflater.inflate(R.layout.lay2, null));
+		listViews.add(mInflater.inflate(R.layout.lay3, null));
+		mPager.setAdapter(new MyPagerAdapter(listViews));
+		mPager.setCurrentItem(0);
+		mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+	}
 
-        @Override
-        public void onClick(View v) {
-            mPager.setCurrentItem(index);
-        }
-    };
-    
-    /**
-     * 页卡切换监听
-     */
-    public class MyOnPageChangeListener implements OnPageChangeListener {
+	/**
+	 * 初始化动画
+	 */
+	private void InitImageView() {
+		cursor = (ImageView) findViewById(R.id.cursor);
+		bmpW = BitmapFactory.decodeResource(getResources(),
+				R.drawable.ic_launcher).getWidth();// 获取图片宽度
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int screenW = dm.widthPixels;// 获取分辨率宽度
+		offset = (screenW / 3 - bmpW) / 2;// 计算偏移量
+		Matrix matrix = new Matrix();
+		matrix.postTranslate(offset, 0);
+		// matrix.postTranslate(0, 0);
+		cursor.setImageMatrix(matrix);// 设置动画初始位置
+	}
 
-    	Animation animation = null;
-    	Matrix matrix = new Matrix();
-    	public MyOnPageChangeListener() {
+	/**
+	 * 头标点击监听
+	 */
+	public class MyOnClickListener implements View.OnClickListener {
+		private int index = 0;
+
+		public MyOnClickListener(int i) {
+			index = i;
 		}
-    	
-        int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
-        int two = one * 2;// 页卡1 -> 页卡3 偏移量
-        int end=0;
-        @Override
-        public void onPageSelected(int arg0) {
-        	  Animation animation = new TranslateAnimation(one * currIndex, one * arg0, 0, 0);// 显然这个比较简洁，只有一行代码。
-              currIndex = arg0;
-              animation.setFillAfter(true);// True:图片停在动画结束位置
-              animation.setDuration(300);
-              cursor.startAnimation(animation);
-        }
 
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-        	switch (arg0) {
-            case 0:
-                if (currIndex == 1) {
-                	
-                } else if (currIndex == 2) {
-                	
-                }
-                break;
-            case 1:
-                if (currIndex == 0) {
-                } else if (currIndex == 2) {
-                }
-                break;
-            case 2:
-                if (currIndex == 0) {
-                } else if (currIndex == 1) {
-                }
-                break;
-            }
-            //matrix.postTranslate(arg2, 0);
-           // cursor.setImageMatrix(matrix);// 设置动画初始位置
-        	
-        	Log.d("onPageScrolled", "------arg0:"+arg0+"arg1:"+arg1+"arg2:"+arg2);
-        }
+		@Override
+		public void onClick(View v) {
+			mPager.setCurrentItem(index);
+		}
+	};
 
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-        }
-    }
-    
+	/**
+	 * 页卡切换监听
+	 */
+	public class MyOnPageChangeListener implements OnPageChangeListener {
+
+		Animation animation = null;
+		Matrix matrix = new Matrix();
+
+		public MyOnPageChangeListener() {
+		}
+
+		int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
+		int two = one * 2;// 页卡1 -> 页卡3 偏移量
+		int end = 0;
+
+		@Override
+		public void onPageSelected(int arg0) {
+			Animation animation = new TranslateAnimation(one * currIndex, one
+					* arg0, 0, 0);// 显然这个比较简洁，只有一行代码。
+			currIndex = arg0;
+			animation.setFillAfter(true);// True:图片停在动画结束位置
+			animation.setDuration(300);
+			cursor.startAnimation(animation);
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+			cursor.setAlpha(1-arg1);
+		/*	if (arg1 <= 0.5) {
+				final AlphaAnimation animation = new AlphaAnimation(1, 1 - arg1);
+				animation.setDuration(1);// 设置动画持续时间
+				// animation.setRepeatCount(int repeatCount);//设置重复次数
+				 animation.setFillAfter(true);//动画执行完后是否停留在执行完的状态
+				// animation.setStartOffset(long startOffset);//执行前的等待时间
+				cursor.setAnimation(animation);
+				animation.startNow();
+			}else{
+				final AlphaAnimation animation = new AlphaAnimation(1, arg1);
+				animation.setDuration(1);// 设置动画持续时间
+				 animation.setFillAfter(true);//动画执行完后是否停留在执行完的状态
+				cursor.setAnimation(animation);
+				animation.startNow();
+			}
+*/
+			Log.d("onPageScrolled", "------arg0:" + arg0 + "arg1:" + arg1
+					+ "arg2:" + arg2);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
+	}
+
 }
